c1d41ba1a9fa51db61f416ea840775f80b64a666
Rootpaceusa <64696314+ahmadchenwangxuesi@users.noreply.github.com>
2020-05-19 12:39:19 +0700

    .

---
+++ js/cookies.js
@@ -0,0 +1,18 @@
+window.addEventListener("load", function(){
+window.cookieconsent.initialise({
+  "palette": {
+    "popup": {
+      "background": "#efefef",
+      "text": "#404040"
+    },
+    "button": {
+      "background": "#8ec760",
+      "text": "#ffffff"
+    }
+  },
+  "theme": "classic",
+  "content": {
+    "message": "This website requires cookies in order to be used without restrictions.",
+    "dismiss": "Understand"
+  }
+})});
